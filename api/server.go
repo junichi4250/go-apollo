b9b98f52155aadc72790124eafd7f7c2d2093d08
katojun <jkky0225@gmail.com>
2022-04-09 15:13:37 +0900

    first commit

---
+++ api/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/junichi4250/go-apollo/graph"
+	"github.com/junichi4250/go-apollo/graph/generated"
+	"github.com/rs/cors"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+
+	// client側localhost:3000からのリクエストを受け付けられるように修正
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowCredentials: true,
+	})
+
+	http.Handle("/query", c.Handler(srv))
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
